b241281252da5405912d9903cfb02a9ea0256740
Kelsey Fecho <kfecho30@gmail.com>
2018-06-15 15:29:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+function scuberGreetingForFeet(dist) {
+  let msg
+  if (dist <= 400) {
+    msg = "This one is one me!";
+  } else if (dist < 2500) {
+    msg = "You owe $30.";
+  } else {
+    msg = "Sorry, we don't go that far."
+  }
+  return msg
+}
