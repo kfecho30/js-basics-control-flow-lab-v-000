132780adb128d3aebe766aee2372d180cc509ae2
Kelsey Fecho <kfecho30@gmail.com>
2018-06-15 15:36:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 // Write your code in this file!
 function scuberGreetingForFeet(dist) {
-  let msg
+  let msg;
   if (dist <= 400) {
     msg = "This one is on me!";
   } else if (dist < 2500) {
     msg = "I will gladly take your thirty bucks.";
   } else {
-    msg = "No can do."
+    msg = "No can do.";
   }
-  return msg
+  return msg;
 }
 
 function ternaryCheckCity(city) {
   let msg = city === 'NYC' ? "Ok, sounds good." : "No go.";
-  return msg
+  return msg;
 }
 
 function switchOnCharmFromTip(tip) {
-  let msg
+  let msg;
   switch(tip) {
     case ('generous'):
       msg = "Thank you so much."
